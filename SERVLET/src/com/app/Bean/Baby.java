dc5e2767b216b4b7a5e41e7eda256c72ec114379
arjunbamoriya535 <arjunkushwah17@gmail.com>
2019-10-09 12:12:00 +0530

    example2

---
+++ SERVLET/src/com/app/Bean/Baby.java
@@ -0,0 +1,44 @@
+package com.app.Bean;
+
+public class Baby {
+
+	int id;
+	String name,meaning,sex,religion;
+	public int getId() {
+		return id;
+		
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+		
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMeaning() {
+		return meaning;
+		
+	}
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+	public String getSex() {
+		return sex;
+		
+	}
+	public String getReligion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public void setReligion(String religion) {
+		this.religion=religion;
+	}
+
+}
