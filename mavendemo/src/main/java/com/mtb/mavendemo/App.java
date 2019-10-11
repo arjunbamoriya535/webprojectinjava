fee16b117e3d246677bb92939128845fd9e1ed4a
arjunbamoriya535 <arjunkushwah17@gmail.com>
2019-10-11 13:42:15 +0530

    second

--- mavendemo/src/main/java/com/mtb/mavendemo/App.java
+++ mavendemo/src/main/java/com/mtb/mavendemo/App.java
@@ -1,5 +1,11 @@
 package com.mtb.mavendemo;
 
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("this my blog i am arjun  singh kushwah  ");
+        Date date=new Date();
+        String strDateformate="hh:mm:ss";
+        DateFormat formate=new SimpleDateFormat(strDateformate);
+        String dateformate=formate.format(date);
+        System.out.println("date is "+dateformate);
+        
     }
 }
